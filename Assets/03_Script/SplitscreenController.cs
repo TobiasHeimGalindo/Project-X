2847a4aafb43785e99b452cc3961d51ca931f1b5
elissa26 <62772808+elissa26@users.noreply.github.com>
2024-05-28 10:00:07 +0200

    Splitscreen TryOut

---
+++ Assets/03_Script/SplitscreenController.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class SplitScreenController : MonoBehaviour
+{
+    private Vector3 _offset;
+    [SerializeField] private Transform target;
+    [SerializeField] private float smoothtime;
+
+    private VEC
+}
